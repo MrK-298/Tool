d392be0d08f10cfe99916cb91e127114bab9ea77
MrK-298 <113235309+MrK-298@users.noreply.github.com>
2024-03-19 22:53:27 +0700

    Add project files.

---
+++ ConsoleApp1/ExamManager.cs
@@ -0,0 +1,37 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace ConsoleApp1
+{
+    public class ExamManager
+    {
+        private readonly IMongoCollection<Exam> _examCollection;
+        public ObjectId examId = ObjectId.Parse("65f99ba5ea8b4587aebef865");
+        public ObjectId questionId;
+        public ExamManager()
+        {
+            var client = new MongoClient("mongodb+srv://khoinguyen29082002:khoibia123@hoangkhoi.9ehzu5m.mongodb.net/");
+            var database = client.GetDatabase("WebTiengAnh");
+            _examCollection = database.GetCollection<Exam>("Exam");
+        }
+
+        public void AddQuestionToExam(Question question)
+        {
+            var filter = Builders<Exam>.Filter.Eq("_id",examId);
+            var update = Builders<Exam>.Update.Push("Questions", question);
+            _examCollection.UpdateOne(filter, update);
+            questionId = question.Id;
+        }
+        public void AddAnswersToQuestion(List<Answer> answers)
+        {
+            var filter = Builders<Exam>.Filter.And(
+                Builders<Exam>.Filter.Eq("_id", examId),
+                Builders<Exam>.Filter.ElemMatch(x => x.Questions, q => q.Id == questionId)
+            );
+
+            var update = Builders<Exam>.Update.Push("Questions.$.Answers", answers);
+
+            _examCollection.UpdateOne(filter, update);
+        }
+    }
+}
---
+++ ConsoleApp1/Question.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Question
+    {
+        public ObjectId Id { get; set; }
+        public string questionText { get; set; }
+    }
+}
